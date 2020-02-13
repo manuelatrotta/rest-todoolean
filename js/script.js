178a2729bc02f48ae1346d4d602946cf8cf0bfe0
manuelatrotta <manuelat88@hotmail.it>
2020-02-13 15:43:36 +0100

    stampa lista su schermo

--- js/script.js
+++ js/script.js
@@ -5,10 +5,19 @@ $(document).ready(function(){
     url: 'http://157.230.17.132:3034/todos',
     method:'GET',
     success: function(data){
+      //variabili standard handlebars
+      var source = $('#entry-template').html();
+      var template = Handlebars.compile(source);
     //ciclo per avere tutti gli elementi della lista
     for(i=0; i<data.length; i++) {
       var element = data[i];
       console.log(element);
+      //stampa elementi in lista tramite template
+      var context = {
+        text: element.text,
+      };
+      var html = template(context);
+      $('.list').append(html);
     }
     },
     error:function(error) {
